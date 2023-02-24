1efc011d61ff385531662ac11b43444e09e2b065
Ubuntu <ubuntu@ip-172-31-70-58.ec2.internal>
2023-02-24 20:39:35 +0000

    Agrego otro test

--- test_funciones.py
+++ test_funciones.py
@@ -2,3 +2,5 @@ from funciones import menor_de_arreglo
 def test_arreglo():
     arreglo = [0, -1, 4, 6000, 1, 2, 1400, -10, 90, 13, 7, 12, 500, 16]
     assert menor_de_arreglo(arreglo) == -10
+    arreglo2 = [0,1,2,3]
+    assert menor_de_arreglo(arreglo2) == -10
